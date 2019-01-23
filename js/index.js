0173c36ca2b19e9090050b0cc8bae18fa7803eff
tiffanynuugen <tiffanynuugen@gmail.com>
2019-01-23 18:33:50 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
+let sally = { name: 'Sally' };
+
+function greet(customerOne, customerTwo) {
+    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
+}
+
+greet.call(sally, 'Terry', 'George');
+
+// let person = {
+//     name: 'Bob',
+//     greet: greet
+// };
+//
+// person.greet();
+//
+//
+// greet.call(sally);
+// greet.apply(sally);
+
+
+
 // use this to copy code snippets or use your browser's console
