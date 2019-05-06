35ec51a76539eebba1bb21b06f5f6f6bbcdb496e
Jcg408 <jcg321@comcas.net>
2019-05-06 13:27:59 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // use this to copy code snippets or use your browser's console
+// use this to copy code snippets or use your browser's console
+function greet() {
+	console.log(`my name is ${this.name}, hi!`);
+}
+
+greet(); // my name is , hi!
+
+let person = {
+	name: 'Bob',
+	greet: greet
+};
+
+person.greet(); // my name is Bob, hi!
+VM4459:2 my name is , hi!
+VM4459:2 my name is Bob, hi!
+undefined
